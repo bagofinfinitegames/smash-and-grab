0c11ff26f593202f1c319f228abe38d90c0f2f17
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-04-05 22:05:09 -0600

    add pickup particles

--- Assets/Scripts/pickup.cs
+++ Assets/Scripts/pickup.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class pickup : MonoBehaviour {
+    public GameObject PickupParticles;
 
     private void OnTriggerEnter(Collider other) {
         Debug.Log("Got Hit!");
         if (other.gameObject.CompareTag("Player")) {
             Debug.Log("Player hit!");
             Destroy(gameObject);
+            Instantiate(PickupParticles, transform.position, Quaternion.identity);
         }
     }
 }
