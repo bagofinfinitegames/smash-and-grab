4d75b4feb4fe6e11d1c744cf1bb54a90556f7c06
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-04-07 11:19:35 -0600

    bug fix player nav agent, add simple rotator and todo list

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -13,6 +13,9 @@ public class player : MonoBehaviour {
             RaycastHit hit;
 
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                if (hit.transform.gameObject.CompareTag("Ground")) {
+                    Debug.Log("Hit the ground");
+                }
                 agent.destination = hit.point;
             }
         }
