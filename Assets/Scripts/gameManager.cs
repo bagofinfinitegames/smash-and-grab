fc1f28ce9a202aad2a4293566e788ec422212047
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-04-13 18:16:14 -0600

    add game manager methods

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -3,14 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class gameManager : MonoBehaviour {
+    public int highScore = 0;
+    public int currentScore = 0;
 
-	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        highScore = loadHighScore();
+    }
+
+    // Returns the saved highscore or 0 if there is no existing save.
+    int loadHighScore() {
+        if (Exists("HighScore")) {
+            return LoadInt("HighScore");
+        } else {
+            return 0;
+        }
+    }
+
+    // Saves the passed score only if it is greater than the last saved score.
+    public void saveHighScore(int highScore) {
+        int curHighScore = LoadInt("HighScore");
+        if(highScore > curHighScore) {
+            SaveInt("HighScore", highScore);
+        }
+    }
+
+    bool Exists(string key) {
+        return PlayerPrefs.HasKey(key);
+    }
+
+    int LoadInt(string key) {
+        return PlayerPrefs.GetInt(key);
+    }
+
+    float LoadFloat(string key) {
+        return PlayerPrefs.GetFloat(key);
+    }
+
+    void SaveInt(string key, int value) {
+        PlayerPrefs.SetInt(key, value);
+    }
+
+    void SaveFloat(string key, float value) {
+        PlayerPrefs.SetFloat(key, value);
+    }
+
+    void ClearSaves() {
+        PlayerPrefs.DeleteAll();
+    }
+
+    void RemoveKey(string keyToRemove) {
+        PlayerPrefs.DeleteKey(keyToRemove);
+    }
 }
