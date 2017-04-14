ceb0ef0823d1c49d9267ecbd2206c1f3247538a5
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-04-14 10:54:40 -0600

    refactor pickup for multiple types

--- Assets/Scripts/pickup.cs
+++ Assets/Scripts/pickup.cs
@@ -3,6 +3,22 @@ using System.Collections;
 
 public class pickup : MonoBehaviour {
     public GameObject PickupParticles;
+    public enum PickupTypes { Health = 1, Ammo = 2, Weapon = 3 };
+    public PickupTypes myType = PickupTypes.Health;
+    public PickupSettings[] myPickupSettings;
+    public Transform myGeometry;
+
+    private void Start() {
+        if(myGeometry == null) {
+            myGeometry = transform.GetChild(0);
+        }
+        Debug.Log(myType);
+        int id = (int)myType;
+        MeshFilter myFilter = myGeometry.GetComponent<MeshFilter>();
+        myFilter.sharedMesh = myPickupSettings[id].mesh;
+        //texture
+        //material
+    }
 
     private void OnTriggerEnter(Collider other) {
         Debug.Log("Got Hit!");
@@ -12,4 +28,14 @@ public class pickup : MonoBehaviour {
             Instantiate(PickupParticles, transform.position, Quaternion.identity);
         }
     }
+}
+
+[System.Serializable]
+public class PickupSettings {
+    public string type;
+    public int value;
+    public Mesh mesh;
+    public Material material;
+    public Texture2D texture;
+    public GameObject particles;
 }
