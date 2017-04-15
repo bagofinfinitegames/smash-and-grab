42904f88f90dadf57543e536f1ba8826537f5bee
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-04-15 11:30:30 -0600

    pickup change texture, puzzle timing and sound, spinner

---
+++ Assets/Scripts/spinner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinner : MonoBehaviour {
+    public float speed = 50.0f;
+    public float offset = 0.333f;
+
+	void Update () {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time, 0.5f) + offset, transform.position.z);
+    }
+}
