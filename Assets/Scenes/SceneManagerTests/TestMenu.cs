7858b1f88cbf422583f25a5bc90ad2672d44594b
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-04-24 15:21:21 -0600

    figure out basic scene management

---
+++ Assets/Scenes/SceneManagerTests/TestMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestMenu : MonoBehaviour {
+    public string curScene = "";
+
+    void Start() {
+        LoadScene("BG_Test", curScene);
+    }
+
+    public void LoadScene(string sceneToLoad) {
+        LoadScene(sceneToLoad, curScene);
+    }
+
+    private void LoadScene(string newScene, string oldScene) {
+        if(curScene != "") {
+            SceneManager.UnloadSceneAsync(oldScene);
+        }
+        SceneManager.LoadScene(newScene, LoadSceneMode.Additive);
+        curScene = newScene;
+    }
+}
