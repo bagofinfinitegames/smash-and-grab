566cf9ac7c9565102e21cf3a8339ae0d795e52b5
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-04-29 19:56:47 -0600

    add patrol functionality

--- Assets/Scripts/Units/enemy.cs
+++ Assets/Scripts/Units/enemy.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class enemy : MonoBehaviour {
     public Transform target;
+    public Transform[] patrolPoints;
+    public int destinationPoint = 0;
     public UnityEngine.AI.NavMeshAgent agent;
     public float viewDistance = 5.0f;
     public int dropRate = 5; // drop rate is 1 in dropRate
     public GameObject drop;
+    //public enum State { wander, attack };
+    //public State state = State.wander;
 
     void Start() {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        GoToCurrentPoint();
+        agent.autoBraking = false;
     }
 
     void Update() {
@@ -17,10 +23,24 @@ public class enemy : MonoBehaviour {
 
         if (distToTarget < viewDistance) {
             agent.destination = target.position;
-        } else {
-            agent.destination = transform.position;
+        } else if (agent.remainingDistance < 0.5f) {
+            GoToNextPoint();
+            //agent.destination = transform.position;
             //agent.Stop();
         }
+
+        //if (agent.remainingDistance < 0.5f) {
+        //    GoToNextPoint();
+        //}
+    }
+
+    void GoToCurrentPoint() {
+        agent.destination = patrolPoints[destinationPoint].position;
+    }
+
+    void GoToNextPoint() {
+        destinationPoint = (destinationPoint + 1) % patrolPoints.Length;
+        agent.destination = patrolPoints[destinationPoint].position;
     }
 
     void Die() {
